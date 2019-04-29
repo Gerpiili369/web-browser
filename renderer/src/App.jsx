12158897d759fa606a1cbaf87a5ee6359eae1d46
Gerpiili369 <elektromiikka@hotmail.com>
2019-04-29 17:22:37 +0300

    Added addressbar hack

--- renderer/src/App.jsx
+++ renderer/src/App.jsx
@@ -3,12 +3,26 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import Tab from './Components/Tab';
+import Toolbar from './Components/Toolbar';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.tab = React.createRef();
+
+        this.setSrc = this.setSrc.bind(this);
+    }
+
+    setSrc(url) {
+        this.tab.current.setSrc(url);
+    }
+
     render() {
         return (
             <div>
-                <Tab url="https://google.com" />
+                <Toolbar activate={ this.setSrc } />
+                <Tab url="https://google.com" ref={ this.tab } />
             </div>
         )
     }
