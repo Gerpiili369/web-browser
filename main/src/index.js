f57925e3680fde4ff1efa64711542a1d31f1bc53
Gerpiili369 <elektromiikka@hotmail.com>
2019-05-04 01:53:45 +0300

    Linter error fixes

--- main/src/index.js
+++ main/src/index.js
@@ -1,10 +1,13 @@
 const
     { app, BrowserWindow } = require('electron'),
     path = require('path');
-let win;
+let win = null;
 
 require('electron-reload')(path.join(__dirname, '..', '..', 'renderer', 'dist', 'bundle.js'));
 
+/**
+ * Crates new window
+ */
 function createWindow() {
     win = new BrowserWindow({
         webPreferences: {
@@ -12,11 +15,13 @@ function createWindow() {
             contextIsolation: false,
         }
     });
-    
+
     win.loadURL(`file://${ __dirname }/../../renderer/dist/index.html`);
     win.webContents.openDevTools();
 
-    win.on('closed', () => win = null);
+    win.on('closed', () => {
+        win = null
+    });
 }
 
 app.on('ready', createWindow);
